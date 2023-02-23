f64df14c4b0347f85637782e876512fb55830238
uicontent <uicontent@mendix.com>
2023-02-23 11:32:14 +0000

    Automatic update of widgets, themes and modules

--- javasource/mendixsso/actions/FindOrCreateUserWithUserInfo.java
+++ javasource/mendixsso/actions/FindOrCreateUserWithUserInfo.java
@@ -61,6 +61,7 @@ public class FindOrCreateUserWithUserInfo extends CustomJavaAction<IMendixObject
 
 	/**
 	 * Returns a string representation of this action
+	 * @return a string representation of this action
 	 */
 	@java.lang.Override
 	public java.lang.String toString()
